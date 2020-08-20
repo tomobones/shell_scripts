87de8e1d5f3aed2f8395b1c8f338484a8a26d9c0
tomobones <thomas.vogg@posteo.de>
2020-08-20 15:21:35 +0200

    to remove an error, added project 'ausgaben' a second time

---
+++ ausgaben/README.md
@@ -0,0 +1,4 @@
+# AUSGABEN
+
+A shell script to monitor my outgoings on my terminal. My basic documentation
+is a csv file, I fill in daily with a smartphone app synchronized by a cloud service. It is a working project with many ideas for improvements.
---
+++ ausgaben/ausgaben.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+# todo
+# - monats details - danach zusammenfassungen einz monate von diesem jahr - dann jahre
+# - liste: betrag rechtsbündig
+# - ausgaben seit neuem monat
+# - mc ausgaben seit letzter rechnung
+# - monatsauflistung nach genre
+# - anteile der genre an gesamtausgaben
+# - laufende ausgaben mit xxxx-xx-25 oder sowas
+
+file="Library/Mobile Documents/27N4MQEA55~pro~writer/Documents/ausgaben.csv"
+
+sum()
+{
+    if [ -z $1 ]
+    then
+        parameter=" "
+    else
+        parameter="$1"
+    fi
+    cat "$HOME"/"$file" |\
+        grep "$parameter" |\
+        cut -d';' -f 3 |\
+        grep -v Betrag |\
+        tr , . |\
+        paste -sd+ - > tmp.txt
+
+    test -s tmp.txt && cat "tmp.txt" | bc || echo "0.00"
+    rm tmp.txt
+}
+
+liste()
+{
+    cat "$HOME"/"$file" |\
+        #grep '#essen' |\
+        cut -d ';' -f 1,3,4,5 |\
+        tr -d '-' |\
+        sed $'s/Betrag/\tBetrag/' |\
+        tr ';' '\t'
+}
+
+if [ -r "$HOME"/"$file" ]
+then
+    printf "\nListe einzelner Ausgaben:\n";liste;printf "\n"
+    printf "Summe aller Ausgaben: ";sum
+    printf "Davon mit Mastercard: ";sum "mc"
+    printf "Davon mit SK RNN:     ";sum "srnn"
+    printf "Davon mit SK München: ";sum "sskm";printf "\n"
+    printf "Essen: .............. ";sum "#essen"
+    printf "Reisen: ............. ";sum "#reise"
+    printf "Bewegung: ........... ";sum "#bewegung";printf "\n"
+
+else
+    echo "file not found"
+fi
+
