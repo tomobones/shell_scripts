25c2d51419fd492115d07291573fa633b63c68a6
Thomas Vogg <thomas.vogg@posteo.de>
2021-02-08 21:18:18 +0100

    removed some scripts

--- helpers/ftp_martin.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-lftp sftp://544640-tomo:`pass 544640-tomo`@hausarzt-altenbach.de
--- helpers/ftp_tomo.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-lftp gsi_7640_1gsi-7640-1:`pass gsi_7640_1gsi-7640-1`@web3.greensta.de
